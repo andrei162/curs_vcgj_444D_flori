a3f16f3eaea5a528f45861a2ae3301ae48afa79d
andrei162 <127126771+andrei162@users.noreply.github.com>
2023-05-12 18:10:04 +0300

    Update test_biblioteca_flori.py

--- app/tests/test_biblioteca_flori.py
+++ app/tests/test_biblioteca_flori.py
@@ -5,8 +5,7 @@ def test_culoare_crin():
     if cul == "alb" : 
         assert True
     else: 
-        assert False
-        
+        assert False 
 
 def test_anotimp_crin():
     cul = bflori.anotimp_crin()
@@ -21,3 +20,24 @@ def test_clasificare_crin():
         assert True
     else: 
         assert False
+        
+def test_culoare_lalea():
+    cul = bflori.culoare_lalea()
+    if cul == "Roz" : 
+        assert True
+    else: 
+        assert False
+        
+def test_anotimp_lalea():
+    cul = bflori.anotimp_lalea()
+    if cul == "Primavara" :
+        assert True
+    else: 
+        assert False
+
+def test_clasificare_lalea():
+    cul = bflori.clasificare_lalea()
+    if cul == "Liliaceae" :
+        assert True
+    else: 
+        assert False        
